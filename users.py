c315f7613087b9b1b76491bc2aeb190e7207b722
Mario Leone <mleone10@gmail.com>
2019-03-27 04:16:08 +0000

    Basic API working

---
+++ users.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import json
+import uuid
+
+def handler(event, context):
+    body = {
+        "api-key": str(uuid.uuid4()),
+        "input": event
+    }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+
+    return response
+
+    # Use this code if you don't use the http event with the LAMBDA-PROXY
+    # integration
+    """
+    return {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "event": event
+    }
+    """
