789567b529e56b1a0d943c4a0cb28db8f4fdfeca
Mario Leone <mleone10@gmail.com>
2019-04-09 03:27:19 +0000

    Fixing stations service so that it returns the user's username from the Cognito user pool

--- stations_service/stations.py
+++ stations_service/stations.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python3
 
 import json
+import jwt
 
 def get(event, context):
+    headerPrefix = 'Bearer '
+    username = str()
+
+    authHeader = event.get('headers', {}).get('Authorization', {})
+
+    if authHeader is not None:
+        idToken = authHeader[len(headerPrefix):]
+        username = jwt.decode(idToken, verify=False).get('cognito:username')
+
     response = {
         "statusCode": 200,
-        "body": "Hello there!"
+        "body": username
     }
 
     return response
